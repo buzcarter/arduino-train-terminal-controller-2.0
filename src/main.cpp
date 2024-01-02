99ca5f6891fcd8a9806acbbad07e2fd61ac5a599
Buz Carter <buzcarter@gmail.com>
2024-01-01 18:00:32 -0800

    DON"T PUSH -- debugging clock LED

--- src/main.cpp
+++ src/main.cpp
@@ -5,44 +5,6 @@
 #include <stationMgr.h>
 #include <TaskScheduler.h>
 
-const int TASK_FREQUENCY = 200; // ms
-
-void checkStationsTask();
-void updateSpeedTask();
-void sendFeedbackTask();
-
-Scheduler taskRunner;
-
-Task stationTask(TASK_FREQUENCY, TASK_FOREVER, &checkStationsTask, &taskRunner, true);
-Task speedTask(TASK_FREQUENCY, TASK_FOREVER, &updateSpeedTask, &taskRunner, true);
-Task feedbackTask(TASK_FREQUENCY, TASK_FOREVER, &sendFeedbackTask, &taskRunner, true);
-
-void checkStationsTask()
-{
-  if (isAtTerminus())
-  {
-    stationTask.disable();
-    reverseDirection();
-    stationTask.enable();
-  }
-  else if (isAtMiddleStation())
-  {
-    stationTask.disable();
-    pauseAndResume();
-    stationTask.enable();
-  }
-}
-
-void updateSpeedTask()
-{
-  updateSpeed();
-}
-
-void sendFeedbackTask()
-{
-  Serial.println("Speed: " + String(getSpeed()) + " " + getDirection());
-}
-
 void setup()
 {
   Serial.begin(9600);
@@ -62,13 +24,34 @@ void setup()
   pinMode(SPEED_MAX_LED_OUT, OUTPUT);
 }
 
+void clockie() {
+  static int clockTick = 0;
+  static bool isOn = false;
+  clockTick++;
+  if (clockTick % 12 == 0) {
+    isOn = !isOn;
+  }
+  digitalWrite(CLOCK_LED_OUT, isOn ? HIGH : LOW);
+
+}
+
 void loop()
 {
-  if (startUpTest())
-  {
-    return;
-  }
+  clockie();
 
-  toggleClockTickIndicator();
-  taskRunner.execute();
+  static bool isOn = false;
+  isOn = !isOn;
+  int value = isOn ? LOW : HIGH;
+
+
+  digitalWrite(FORWARD_LED_OUT, value);
+  digitalWrite(LAYOVER_LED_OUT, value);
+
+  digitalWrite(SPEED_MIN_LED_OUT, ON);
+  digitalWrite(SPEED_20_LED_OUT, ON);
+  digitalWrite(SPEED_40_LED_OUT, ON);
+  digitalWrite(SPEED_80_LED_OUT, ON);
+  digitalWrite(SPEED_MAX_LED_OUT, ON);
+
+  delay(100);
 }
